7f7d6baa3f2179185aa2bea069b278aed66bb17c
Cyber <ekiller886@163.com>
2014-07-13 21:52:01 +0800

    Add one class to store the common symbols using in the project.

---
+++ src/main/java/com/bankbusiness/common/SymbolPool.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright $copy; 2014 Cyber. All rights reserved.
+ */
+package com.bankbusiness.common;
+
+/**
+ * This pool stores the common symbol used in the project. * 
+ * @author Cyber
+ */
+public class SymbolPool 
+{
+	public static String COLON = ":";
+	public static String HYTHEN = "-";
+	public static String WHITESPACE = " ";
+	public static String COMMA = ",";
+}
