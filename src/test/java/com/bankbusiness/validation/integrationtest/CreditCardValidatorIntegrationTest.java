7d3927a9a1f0db8368b2ea0c0291551191fd6db2
Cyber <ekiller886@163.com>
2014-07-15 01:57:38 +0800

    Remove the unuseful package

--- src/test/java/com/bankbusiness/validation/integrationtest/CreditCardValidatorIntegrationTest.java
+++ src/test/java/com/bankbusiness/validation/integrationtest/CreditCardValidatorIntegrationTest.java
@@ -14,7 +14,6 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
